73c9d2607a89d528f85688a4b621dd52abb4b113
Gd. Megh <gdmegh@gmail.com>
2025-07-23 07:40:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/vanta-background.tsx
+++ src/components/vanta-background.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
@@ -56,7 +57,7 @@ const VantaBackground = () => {
     return () => {
       if (vantaEffect) vantaEffect.destroy();
     };
-  }, []);
+  }, [vantaEffect]);
 
   return <div ref={vantaRef} className="absolute inset-0 z-0" />;
 };
