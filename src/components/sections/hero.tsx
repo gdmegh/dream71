5546d04440e23b355ca9281d86eaf45fe26f19d5
Gd. Megh <gdmegh@gmail.com>
2025-07-23 09:57:21 +0000

    image corner redius

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,39 +1,10 @@
-"use client";
-
-import { useRef } from 'react';
 import { Braces } from 'lucide-react';
-import HALO from 'vanta/dist/vanta.halo.min';
-import * as THREE from 'three';
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 export default function Hero() {
-    const vantaRef = useRef(null);
-    const [vantaEffect, setVantaEffect] = useState<any>(0);
-
-    useEffect(() => {
-        if (!vantaEffect && vantaRef.current) {
-            setVantaEffect(HALO({
-                el: vantaRef.current,
-                THREE: THREE,
-                mouseControls: true,
-                touchControls: true,
-                gyroControls: false,
-                minHeight: 200.00,
-                minWidth: 200.00,
-                backgroundColor: 0x011304,
-                size: 1.5,
-            }));
-        }
-        return () => {
-            if (vantaEffect) vantaEffect.destroy();
-        };
-    }, [vantaEffect]);
-
     return (
         <section
             id="home"
-            ref={vantaRef}
             className="relative flex flex-col items-center justify-center text-center overflow-hidden bg-background min-h-screen py-20"
         >
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10 flex flex-col items-center flex-grow">
@@ -48,7 +19,7 @@ export default function Hero() {
                         alt="Hero Image"
                         width={1200}
                         height={600}
-                        className="rounded-t-lg shadow-2xl transition-transform duration-500 ease-in-out group-hover:scale-105 w-full"
+                        className="rounded-lg shadow-2xl transition-transform duration-500 ease-in-out group-hover:scale-105 w-full"
                         data-ai-hint="software development"
                     />
                 </div>
