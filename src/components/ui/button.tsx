6d187c1a484649e0835081c509e124a8af14491a
Gd. Megh <gdmegh@gmail.com>
2025-07-24 06:44:10 +0000

    btn should be bit width

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,9 +21,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 px-3",
-        lg: "h-11 px-8",
+        default: "h-10 px-6 py-2",
+        sm: "h-9 px-4",
+        lg: "h-11 px-10",
         icon: "h-10 w-10",
       },
     },
