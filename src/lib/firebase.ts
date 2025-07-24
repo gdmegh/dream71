546f0c621c771316c77ff77931194bb8b5ba96b1
Gd. Megh <gdmegh@gmail.com>
2025-07-24 14:57:33 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "FIREBASE_API_KEY",
-  authDomain: "dream71.firebaseapp.com",
-  projectId: "dream71",
-  storageBucket: "dream71.appspot.com",
-  messagingSenderId: "47285641151",
-  appId: "1:47285641151:web:2120e55722421297e64177"
+  apiKey: "AIzaSyBToN849udrajLB1LN8fWt75OCcVp4jcIs",
+  authDomain: "megh-gallery.firebaseapp.com",
+  projectId: "megh-gallery",
+  storageBucket: "megh-gallery.appspot.com",
+  messagingSenderId: "645292722370",
+  appId: "1:645292722370:web:7641980429ab0e4e8b535b"
 };
 
 // Initialize Firebase
