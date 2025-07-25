ab207c8fbfdab87c40f044a4e844906332f0f96b
Gd. Megh <gdmegh@gmail.com>
2025-07-25 11:06:06 +0000

    replace node.js to php icon

--- src/components/ui/animated-icons.tsx
+++ src/components/ui/animated-icons.tsx
@@ -14,11 +14,10 @@ const ReactIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 );
 
-const NodeJSIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#8CC84B" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="M12 2L2 7l10 5 10-5-10-5z" />
-      <path d="M2 17l10 5 10-5" />
-      <path d="M2 12l10 5 10-5" />
+const PHPIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 78" {...props}>
+        <path fill="#777BB4" d="M64 78c-23.23 0-42.06-18.83-42.06-42.06S40.77 0 64 0s42.06 18.83 42.06 42.06S87.23 78 64 78z"/>
+        <path fill="#FFF" d="M83.52 38.61c-4.32 0-7.36 2.08-9.08 5.75h-.13c-.32-1.89-.58-3.69-.58-5.75v-18.1h-10.4v39.52h8.56v-12.9c0-5.75 1.54-9.39 6.22-9.39 3.9 0 5.88 2.87 5.88 8.1v14.19h10.4V46.7c.02-6.53-2.73-10.17-10.87-10.17l-.01 2.08zm-32.06-2.08c-4.32 0-7.36 2.08-9.08 5.75h-.13c-.32-1.89-.58-3.69-.58-5.75v-18.1h-10.4v39.52h8.56v-12.9c0-5.75 1.54-9.39 6.22-9.39 3.9 0 5.88 2.87 5.88 8.1v14.19h10.4V46.7c0-6.53-2.74-10.17-10.87-10.17zm-32-2.08c-4.32 0-7.36 2.08-9.08 5.75h-.13c-.32-1.89-.58-3.69-.58-5.75v-18.1H.28v39.52h8.56v-12.9c0-5.75 1.54-9.39 6.22-9.39 3.9 0 5.88 2.87 5.88 8.1v14.19H31.4V46.7c0-6.53-2.74-10.17-10.88-10.17l-.02 2.08z"/>
     </svg>
 );
 
@@ -36,7 +35,7 @@ export default function AnimatedIcons() {
              <div
                 className="absolute bottom-[10%] right-[5%]"
             >
-                <NodeJSIcon className="w-16 h-16 opacity-50" />
+                <PHPIcon className="w-16 h-16 opacity-50" />
             </div>
         </div>
     );
