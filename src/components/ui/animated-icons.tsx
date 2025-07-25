bb510266b5ec7a2f3f3ca8e6f22fab725f655190
Gd. Megh <gdmegh@gmail.com>
2025-07-25 10:53:01 +0000

    keep only left top react icon others remove, can you make the react icon

--- src/components/ui/animated-icons.tsx
+++ src/components/ui/animated-icons.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const ReactIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348" {...props}>
@@ -13,24 +14,17 @@ const ReactIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 );
 
-const icons = [
-    { Icon: ReactIcon, className: 'absolute top-[10%] left-[15%] w-16 h-16' },
-    { Icon: ReactIcon, className: 'absolute bottom-[15%] right-[20%] w-20 h-20' },
-    { Icon: ReactIcon, className: 'absolute top-[25%] right-[25%] w-12 h-12' },
-    { Icon: ReactIcon, className: 'absolute bottom-[5%] left-[30%] w-16 h-16' },
-];
 
 export default function AnimatedIcons() {
     return (
         <div className="absolute inset-0 w-full h-full overflow-hidden z-0 pointer-events-none">
-            {icons.map((item, i) => {
-                const { Icon, className } = item;
-                return (
-                    <div key={i} className={className}>
-                        <Icon />
-                    </div>
-                );
-            })}
+             <motion.div
+                className="absolute top-[10%] left-[15%]"
+                animate={{ rotate: 360 }}
+                transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
+            >
+                <ReactIcon className="w-16 h-16" />
+            </motion.div>
         </div>
     );
 }
